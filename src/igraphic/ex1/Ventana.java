f665eebec286329c99b5ecb8b4d72fa9229f476b
Juan Galvis <Galvis>
2024-10-24 09:57:50 -0500

    Add title and close the window after exit

--- src/igraphic/ex1/Ventana.java
+++ src/igraphic/ex1/Ventana.java
@@ -5,6 +5,8 @@ import javax.swing.*;
 public class Ventana extends JFrame {
 
     public Ventana() {
+        this.setTitle("El mejor titulo");
         this.setSize(500, 500);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 }
