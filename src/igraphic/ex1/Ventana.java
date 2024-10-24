ff7eeb519f88e09ecea60e08585cd6e712b54767
Juan Galvis <Galvis>
2024-10-24 10:06:01 -0500

    Use SetLocation, SetBounds to the JFrame

--- src/igraphic/ex1/Ventana.java
+++ src/igraphic/ex1/Ventana.java
@@ -5,8 +5,11 @@ import javax.swing.*;
 public class Ventana extends JFrame {
 
     public Ventana() {
-        this.setTitle("El mejor titulo");
-        this.setSize(500, 500);
+        this.setTitle("Test Title");
+        //this.setBounds(200, 200, 600, 600);
+        //this.setLocation(100,200);
+        this.setSize(500,500);
+        this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 }
