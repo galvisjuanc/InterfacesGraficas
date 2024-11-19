c32cbaa116e5b310531919abe84645627741121e
Juan Galvis <Galvis>
2024-11-19 09:27:33 -0500

    Rename VentanaEx and change labels

--- src/igraphic/ex2/VentanaEx.java
+++ src/igraphic/ex2/VentanaEx.java
@@ -3,13 +3,13 @@ package igraphic.ex2;
 import javax.swing.*;
 import java.awt.*;
 
-public class Ventana extends JFrame {
+public class VentanaEx extends JFrame {
     private JPanel jPanel;
     private JLabel jLabel;
     private JTextField jTextField;
     private JButton jButton1;
     
-    public Ventana () {
+    public VentanaEx() {
         setBounds(50,50,500, 500);
         setTitle("Eventos");
         
@@ -32,11 +32,10 @@ public class Ventana extends JFrame {
     }
 
     private void colocarEtiqueta() {
-        jLabel = new JLabel();
-        jLabel.setText("Ingrese su nombre: ");
-        jLabel.setBounds(30,10,200,30);
-        jLabel.setFont(new Font("cooper black", Font.PLAIN, 30));
-        jPanel.add(jLabel);
+        JLabel etiqueta = new JLabel("Ingrese su nombre: ");
+        etiqueta.setBounds(30,10,200,30);
+        etiqueta.setFont(new Font("cooper black", Font.PLAIN, 30));
+        jPanel.add(etiqueta);
     }
 
     private void colocarCajaDeTexto() {
@@ -46,6 +45,11 @@ public class Ventana extends JFrame {
     }
 
     private void colocarBoton() {
+        jButton1 = new JButton();
+        jButton1.setText("¡Pulsa Aquí!");
+        jButton1.setBounds(150,100,150,40);
+        jButton1.setFont(new Font("arial", Font.PLAIN, 15));
+        jPanel.add(jButton1);
     }
 
 }
