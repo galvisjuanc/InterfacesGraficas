724d5d2a04ee6e0c256cccd3cabee35475f92da8
Juan Galvis <Galvis>
2024-11-28 10:26:23 -0500

    Principal Ex5

---
+++ src/igraphic/ex5/Principal.java
@@ -0,0 +1,9 @@
+package igraphic.ex5;
+
+public class Principal {
+    public static void main(String[] args) {
+        VentanaEx5 ventana = new VentanaEx5();
+        ventana.setVisible(true);
+
+    }
+}
