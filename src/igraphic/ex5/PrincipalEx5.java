13755f1696be40ef05128a6467e3aecd5ec511c0
Juan Galvis <Galvis>
2024-11-28 10:31:48 -0500

    Rename Principal Ex5

--- src/igraphic/ex5/PrincipalEx5.java
+++ src/igraphic/ex5/PrincipalEx5.java
@@ -1,6 +1,6 @@
 package igraphic.ex5;
 
-public class Principal {
+public class PrincipalEx5 {
     public static void main(String[] args) {
         VentanaEx5 ventana = new VentanaEx5();
         ventana.setVisible(true);
