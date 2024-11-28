44118c3ef264cb6c176f60a9679e6e514b335a2b
Juan Galvis <Galvis>
2024-11-27 19:45:50 -0500

    Tweak ScrollPane

--- src/igraphic/ex4/VentanaEx4.java
+++ src/igraphic/ex4/VentanaEx4.java
@@ -39,8 +39,6 @@ public class VentanaEx4 extends JFrame {
 
         JScrollPane jScrollPane = new JScrollPane(jTextArea, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         jScrollPane.setBounds(20, 20, 200, 300);
-        //jScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
-        //jScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         jPanel.add(jScrollPane);
     }
 
