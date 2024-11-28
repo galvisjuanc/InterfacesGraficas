1c43b4e1785dafafed92d801f7d7127abef79e3a
Juan Galvis <Galvis>
2024-11-28 10:20:56 -0500

    Ventana Ex5

---
+++ src/igraphic/ex5/VentanaEx5.java
@@ -0,0 +1,58 @@
+package igraphic.ex5;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+
+public class VentanaEx5 extends JFrame {
+
+    private JPanel jPanel;
+    private JTextArea jTextArea;
+    private JButton jButton1;
+
+    public VentanaEx5() {
+        setBounds(50,50,600,600);
+        setTitle("Eventos del Mouse Listener - Mouse Motion");
+        iniciarComponentes();
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    private void iniciarComponentes() {
+        colocarPanel();
+        colocarAreaTexto();
+        colocarBoton();
+    }
+
+    private void colocarPanel() {
+        jPanel = new JPanel();
+        jPanel.setLayout(null);
+        this.add(jPanel);
+
+        eventoRuedaMovimientoRaton();
+    }
+
+    private void colocarAreaTexto() {
+        jTextArea = new JTextArea();
+        jTextArea.setBounds(20,20,200,300);
+        jPanel.add(jTextArea);
+
+        JScrollPane jScrollPane = new JScrollPane(jTextArea, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        jScrollPane.setBounds(20, 20, 200, 300);
+        jPanel.add(jScrollPane);
+    }
+
+    private void colocarBoton() {
+        jButton1 = new JButton();
+        jButton1.setText("¡Pulsa aquí!");
+        jButton1.setBounds(150,350,150,40);
+        jButton1.setFont(new Font("Arial", Font.PLAIN, 15));
+        jPanel.add(jButton1);
+
+
+    }
+
+    private void eventoRuedaMovimientoRaton() {
+        jPanel.addMouseWheelListener(null);
+    }
+}
