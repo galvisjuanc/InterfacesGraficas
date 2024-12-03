28e8db6b4bf8f2f56f62af65b3338d09bc999081
Juan Galvis <Galvis>
2024-12-03 11:10:15 -0500

    KeyListener I

---
+++ src/igraphic/ex6/PrincipalEx6.java
@@ -0,0 +1,8 @@
+package igraphic.ex6;
+
+public class PrincipalEx6 {
+    public static void main(String[] args) {
+        VentanaEx6 ventana = new VentanaEx6();
+        ventana.setVisible(true);
+    }
+}
