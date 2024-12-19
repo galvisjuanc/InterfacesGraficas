7ad7d3cd8e9a36ba3fe0cf6546ca77b32d4808db
Juan Galvis <Galvis>
2024-12-19 15:40:06 -0500

    Principal Main for Ex 1

---
+++ src/igraphic/ex7/PrincipalEx7.java
@@ -0,0 +1,8 @@
+package igraphic.ex7;
+
+public class PrincipalEx7 {
+    public static void main(String[] args) {
+        VentanaEx7 ventana = new VentanaEx7();
+        ventana.setVisible(true);
+    }
+}
