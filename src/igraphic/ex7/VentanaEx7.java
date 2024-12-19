1696dd23b0d86ddb40fe33b15ad6e91329c9d4c3
Juan Galvis <Galvis>
2024-12-19 15:38:03 -0500

    Tweak to delete old libraries

--- src/igraphic/ex7/VentanaEx7.java
+++ src/igraphic/ex7/VentanaEx7.java
@@ -2,10 +2,7 @@ package igraphic.ex7;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
 public class VentanaEx7 extends JFrame {
 
