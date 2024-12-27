25bc9e2fc7513c223e85481b27bee4f51d435a21
Juan Galvis <Galvis>
2024-12-27 15:56:25 -0500

    Tweak to the JFrame title

--- src/igraphic/ex8/VentanaEx8.java
+++ src/igraphic/ex8/VentanaEx8.java
@@ -12,7 +12,7 @@ public class VentanaEx8 extends JFrame {
 
     public VentanaEx8() {
         setSize(900,900);
-        setTitle("Selección del Deporte favorito");
+        setTitle("Selección del deporte favorito");
         setLocationRelativeTo(null);
         iniciarComponentes();
         setDefaultCloseOperation(EXIT_ON_CLOSE);
