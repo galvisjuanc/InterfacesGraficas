d15c845c0e673ea43aa3ae3ddcdcad4a9767aedb
Juan Galvis <Galvis>
2025-01-01 11:42:30 -0500

    Third Project I

--- src/igraphic/ex9/PrincipalEx9.java
+++ src/igraphic/ex9/PrincipalEx9.java
@@ -1,4 +1,8 @@
 package igraphic.ex9;
 
 public class PrincipalEx9 {
+    public static void main(String[] args) {
+        VentanaEx9 ventana = new VentanaEx9();
+        ventana.setVisible(true);
+    }
 }
