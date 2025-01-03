7173cdcb5a4e5fe47b3f6be7808627e6c093b0f4
Juan Galvis <Galvis>
2025-01-03 15:41:24 -0500

    Include labels for the colors

--- src/igraphic/ex9/VentanaEx9.java
+++ src/igraphic/ex9/VentanaEx9.java
@@ -1,8 +1,13 @@
 package igraphic.ex9;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class VentanaEx9 extends JFrame {
+
+    private JPanel panel;
+    private JLabel label;
+
     public VentanaEx9() {
         setSize(600,400);
         setTitle("Colores de Ventana");
@@ -15,11 +20,20 @@ public class VentanaEx9 extends JFrame {
 
     private void iniciarComponentes() {
         colocarPanel();
+        colocarEtiqueta();
     }
 
     private void colocarPanel() {
-        JPanel panel = new JPanel();
+        panel = new JPanel();
         panel.setLayout(null);
         this.add(panel);
+    }
+
+    private void colocarEtiqueta() {
+        label = new JLabel("Color (Rojo, Verde, Azul)");
+        label.setBounds(100, 30, 400, 50);
+        label.setFont(new Font("Arial", Font.BOLD, 20));
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+        panel.add(label);
     }
 }
