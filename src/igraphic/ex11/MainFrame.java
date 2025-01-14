6eba787d5f5b5d4e19f4fdab88e915ca72e63317
Juan Galvis <Galvis>
2025-01-14 09:04:16 -0500

    Create a form with Grid Layout

---
+++ src/igraphic/ex11/MainFrame.java
@@ -0,0 +1,11 @@
+package igraphic.ex11;
+
+import javax.swing.*;
+
+public class MainFrame {
+    private JPanel mainPanel;
+    private JTextField tfFirstName;
+    private JTextField tfLastName;
+    private JButton btnOK;
+    private JButton btnClear;
+}
