7846c854bd915cec1c2cd3f9d6b3d17ba2a44e44
Juan Galvis <Galvis>
2025-01-14 09:20:19 -0500

    Add Listeners according to the request

--- src/igraphic/ex11/MainFrame.java
+++ src/igraphic/ex11/MainFrame.java
@@ -1,6 +1,8 @@
 package igraphic.ex11;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainFrame extends JFrame {
     private JPanel mainPanel;
@@ -16,6 +18,21 @@ public class MainFrame extends JFrame {
         setSize(450,300);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
+        buttonEvents();
+    }
+
+    private void buttonEvents() {
+        btnOK.addActionListener(e -> {
+            String firstName = tfFirstName.getText();
+            String lastName = tfLastName.getText();
+            lbWelcome.setText("Welcome " + firstName + " " + lastName);
+        });
+
+        btnClear.addActionListener(e -> {
+            tfFirstName.setText("");
+            tfLastName.setText("");
+            lbWelcome.setText("");
+        });
     }
 
     public static void main(String[] args) {
