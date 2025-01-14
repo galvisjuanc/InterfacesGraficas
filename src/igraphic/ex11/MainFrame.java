59100262301dc9a2d7a7690dc0e1e32da2f9a2ba
Juan Galvis <Galvis>
2025-01-14 09:11:59 -0500

    Add logic to the interface grafica

--- src/igraphic/ex11/MainFrame.java
+++ src/igraphic/ex11/MainFrame.java
@@ -2,10 +2,22 @@ package igraphic.ex11;
 
 import javax.swing.*;
 
-public class MainFrame {
+public class MainFrame extends JFrame {
     private JPanel mainPanel;
     private JTextField tfFirstName;
     private JTextField tfLastName;
     private JButton btnOK;
     private JButton btnClear;
+    private JLabel lbWelcome;
+
+    public MainFrame() {
+        setTitle("Welcome");
+        setSize(450,300);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        MainFrame mainFrame = new MainFrame();
+    }
 }
