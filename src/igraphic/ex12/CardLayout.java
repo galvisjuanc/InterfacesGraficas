0eb24708686ac62e1b2b39a527519d07d861d4d4
Mauricio Coral <Coral>
2025-01-16 10:13:56 -0500

    Add CardLayout Example

---
+++ src/igraphic/ex12/CardLayout.java
@@ -0,0 +1,12 @@
+package igraphic.ex12;
+
+import javax.swing.*;
+
+public class CardLayout {
+    private JPanel mainPanel;
+    private JPanel buttonsPanel;
+    private JPanel parentPanel;
+    private JButton card1Button;
+    private JButton card2Button;
+    private JButton card3Button;
+}
