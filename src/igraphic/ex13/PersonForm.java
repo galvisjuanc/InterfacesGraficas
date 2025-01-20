18c61f13d2b84afe611658a47a5f50580317a42f
Juan Galvis <Galvis>
2025-01-20 09:24:12 -0500

    Create an interface for people using GUI

---
+++ src/igraphic/ex13/PersonForm.java
@@ -0,0 +1,22 @@
+package igraphic.ex13;
+
+import javax.swing.*;
+
+public class PersonForm {
+    private JPanel mainPanel;
+    private JLabel lbPersona;
+    private JComboBox comboBox1;
+    private JButton btnInsertar;
+    private JTable table1;
+    private JLabel lbDatosPersona;
+    private JTextField txfdNombre;
+    private JTextField txfdApellidos;
+    private JLabel lbNombre;
+    private JLabel lbApellido;
+    private JTextField txfdEmail;
+    private JTextField txfdCelular;
+    private JTextField txfdDireccion;
+    private JLabel lbEmail;
+    private JLabel lbCelular;
+    private JLabel lbDireccion;
+}
