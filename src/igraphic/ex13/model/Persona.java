8c766d80ea811160aa3a61eba1fa31d5fbfa400c
Juan Galvis <Galvis>
2025-01-20 09:52:09 -0500

    Data of the people

---
+++ src/igraphic/ex13/model/Persona.java
@@ -0,0 +1,17 @@
+package igraphic.ex13.model;
+
+public class Persona {
+    private String nombre;
+    private String apellidos;
+    private String email;
+    private String celular;
+    private String direccion;
+
+    public Persona(String nombre, String apellidos, String email, String celular, String direccion) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.email = email;
+        this.celular = celular;
+        this.direccion = direccion;
+    }
+}
