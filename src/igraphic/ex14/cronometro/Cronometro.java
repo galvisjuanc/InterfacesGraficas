312c542d475207249b9080a7e182224c7fee294f
Juan Galvis <Galvis>
2025-01-28 12:45:29 -0500

    Third part of the chronometer

--- src/igraphic/ex14/cronometro/Cronometro.java
+++ src/igraphic/ex14/cronometro/Cronometro.java
@@ -1,14 +1,18 @@
 package igraphic.ex14.cronometro;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class Cronometro extends JFrame{
     private JPanel mainPanel;
-    private JLabel jLabelDesk;
+    private JLabel mainLabel;
+    private JButton btnIniciar;
+    private JButton btnPausar;
+    private JButton btnReiniciar;
+    private JLabel valorCronometro;
 
     public Cronometro() {
         setContentPane(mainPanel);
+        setResizable(false);
         setTitle("Cronometro");
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
