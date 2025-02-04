37904b279537fd0b87a07c3b594fdb8ad7907823
Juan Galvis <Galvis>
2025-02-04 12:23:38 -0500

    Inicial Commit with Files

---
+++ src/files/Ex1/FileOfExample.java
@@ -0,0 +1,9 @@
+package files.Ex1;
+
+import java.io.File;
+
+public class FileOfExample {
+    public static void main(String[] args) {
+        File archivo = new File("C:\\Users\\galvi\\OneDrive\\Documentos\\Java Projects\\InterfacesGraficas\\src\\files\\Ex1\\file.txt");
+    }
+}
