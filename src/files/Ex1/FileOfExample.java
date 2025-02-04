e317b43ed0977467b8e5ba0b9e8d16d25054c1e9
Juan Galvis <Galvis>
2025-02-04 12:38:56 -0500

    Include a file.txt and verify the behaviour of files

--- src/files/Ex1/FileOfExample.java
+++ src/files/Ex1/FileOfExample.java
@@ -4,10 +4,13 @@ import java.io.File;
 
 public class FileOfExample {
     public static void main(String[] args) {
-        File archivo = new File("C:\\Users\\galvi\\OneDrive\\Documentos\\Java Projects\\InterfacesGraficas\\src\\files\\Ex1\\file.txt");
+        File archivo = new File("file.txt");
 
         if (archivo.exists()) {
             System.out.println("El archivo si existe.");
+            System.out.println(archivo.getAbsolutePath());
+            System.out.println("¿Es un archivo? --> " + archivo.isFile());
+            System.out.println("¿Se puede leer? --> " + archivo.canRead());
         } else {
             System.out.println("El archivo no existe.");
         }
