ffb86f4dd8de96ef0a8610c670c64c9fb2a41c0c
Juan Galvis <Galvis>
2025-02-10 09:35:42 -0500

    Add an file class

---
+++ src/igraphic/ex15/Archivo.java
@@ -0,0 +1,20 @@
+package igraphic.ex15;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Archivo {
+    private File archivo;
+
+    public void crearArchivo() {
+        archivo = new File("agendaContactos.txt");
+
+        try {
+            if(archivo.createNewFile()) {
+
+            }
+        } catch (IOException e) {
+            System.err.println("No se pudo crear el archivo. Error: " + e.getMessage());
+        }
+    }
+}
