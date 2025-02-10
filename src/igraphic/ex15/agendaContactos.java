eb84cf41384fd96be7ffb1d1fd902dbc54548a35
Juan Galvis <Galvis>
2025-02-10 09:00:41 -0500

    Create a contact agenda

---
+++ src/igraphic/ex15/agendaContactos.java
@@ -0,0 +1,13 @@
+package igraphic.ex15;
+
+import javax.swing.*;
+
+public class agendaContactos {
+    private JToolBar barraMenu;
+    private JLabel lbTitle;
+    private JLabel lbnNombre;
+    private JTextField textFieldName;
+    private JButton btnGuardar;
+    private JTextField textFieldCorreo;
+    private JTextField textFieldCelular;
+}
