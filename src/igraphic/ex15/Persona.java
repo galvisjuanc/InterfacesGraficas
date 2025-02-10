203bf60e7bee006c22d6e11e1be2f9ab1c57d0df
Juan Galvis <Galvis>
2025-02-10 09:27:33 -0500

    Add a Persona class

---
+++ src/igraphic/ex15/Persona.java
@@ -0,0 +1,13 @@
+package igraphic.ex15;
+
+public class Persona {
+    private String nombre;
+    private String correo;
+    private String celular;
+
+    public Persona(String nombre, String correo, String celular) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.celular = celular;
+    }
+}
