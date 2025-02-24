b0f5a7c275ca58c78d78152d308d0fc73b20f107
Juan Galvis <Galvis>
2025-02-24 07:59:56 -0500

    Finishing the writing bin method

--- src/files/Ex3/ArchivosBinarios.java
+++ src/files/Ex3/ArchivosBinarios.java
@@ -11,6 +11,16 @@ public class ArchivosBinarios {
         try (FileOutputStream archivo = new FileOutputStream("personas.bin")) {
             ObjectOutputStream escritura = new ObjectOutputStream(archivo);
 
+            PersonBin personBin1 = new PersonBin("Juan", 32);
+            PersonBin personBin2 = new PersonBin("Cossette", 34);
+            PersonBin personBin3 = new PersonBin("María José", 17);
+
+            escritura.writeObject(personBin1);
+            escritura.writeObject(personBin2);
+            escritura.writeObject(personBin3);
+
+            escritura.close();
+
         } catch (IOException e) {
             System.err.println("No se pudo escribir el archivo. Error: " + e.getMessage());
         }
